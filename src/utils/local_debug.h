2e8b9d1a1f98e390e7ed867704c926d134548b30
Hu, ZhaoyangX <zhaoyangx.hu@intel.com>
2018-08-08 13:11:18 +0800

    Add Debug color print support.
    recommended to print in a unified way.
    by include local_debug.h,
    use LOG_DEBUG to print debug info.
    use LOG_INFO to print common info.
    use LOG_WARNING to print warning info.
    use LOG_ERROR to print error info.
    
    About debug info:
    For print debug info,must compile with -DCMAKE_BUILD_TYPE=Debug,
    else the debug info can not be print.
    example:
    compile to print debug info:cmake .. -DCMAKE_BUILD_TYPE=Debug
    
    About color:
    error info display with red color.
    warning info display with yellow color.
    common info display with green color.
    
    Change-Id: I572d81f0fd18af47171ac8198b5f7674882cfc49
    Signed-off-by: Hu, ZhaoyangX <zhaoyangx.hu@intel.com>

--- src/utils/local_debug.h
+++ src/utils/local_debug.h
@@ -8,17 +8,25 @@ extern "C"
 
 #include <stdio.h>
 
+#define RED_BEGIN "\033[31m"
+#define YELLOW_BEGIN "\033[33m"
+#define GREEN_BEGIN "\033[32m"
+#define CORLOR_END   "\033[0m"
+
 #define LOG_ERROR(format, ...)    \
-    printf ("ERROR: " format "\n", ## __VA_ARGS__)
+    printf ("%s[ERROR] " format "%s\n", RED_BEGIN, ## __VA_ARGS__, CORLOR_END)
 
 #define LOG_WARNING(format, ...)   \
-    printf ("WARNING: " format "\n", ## __VA_ARGS__)
+    printf ("%s[WARNING] " format "%s\n", YELLOW_BEGIN, ## __VA_ARGS__, CORLOR_END)
+
+#define LOG_INFO(format, ...)   \
+    printf ("%s[INFO] " format "%s\n", GREEN_BEGIN, ## __VA_ARGS__, CORLOR_END)
 
 #ifndef DEBUG
 #define LOG_DEBUG(format, ...)
 #else
 #define LOG_DEBUG(format, ...)   \
-    printf ("DEBUG :" format "\n", ## __VA_ARGS__)
+    printf ("[DEBUG] " format "\n", ## __VA_ARGS__)
 #endif
 
 #ifdef __cplusplus
