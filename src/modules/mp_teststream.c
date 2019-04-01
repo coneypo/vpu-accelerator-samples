5bcb9d92cc6a5bfe736045569cc03dff548c8ac1
Zhou, Shuangpeng <shuangpeng.zhou@intel.com>
2019-04-01 10:42:55 +0800

    Remove unused code & Refactor code

--- src/modules/mp_teststream.c
+++ src/modules/mp_teststream.c
@@ -7,61 +7,8 @@
 #include <vector>
 
 typedef struct {
-    GstElement *decode_pipeline;
-    char data_head[1024];
-    int pkg_offset;
-    GstBuffer *half_buffer;
-    guint64 total_offset;
-    GQueue *meta_queue;
+    GQueue* meta_queue;
 } stream_ctx_t;
-
-/* This structure is GstVideoRegionOfInterestMeta's params */
-typedef struct {
-    /*header*/
-    guint8  magic;
-    guint8  header_version;
-    guint16 meta_size;
-    guint32 package_size;
-
-    /*Metadata*/
-    guint8  meta_version;
-    guint8  packet_type;
-    guint8  stream_id;
-    guint8  of_objects;
-    guint32 frame_number;
-
-    /*object*/
-    guint8  reserved;
-    guint8  object_id;
-    guint16 classfication_GT;
-} param_struct_t;
-
-#pragma pack(push)
-#pragma pack(1)
-typedef struct {
-    unsigned u : 24;
-} foo_t;
-#pragma pack(pop)
-
-typedef struct {
-    /*object*/
-    guint8  reserved;
-    guint8  object_id;
-    guint16 classfication_GT;
-    foo_t left;
-    foo_t top;
-    foo_t width;
-    foo_t height;
-} object_border_t;
-
-typedef struct {
-    /*object*/
-    guint x;
-    guint y;
-    guint w;
-    guint h;
-    param_struct_t  *param_data;
-} private_metadata_t;
 
 typedef struct {
     guint8 magic;
@@ -95,92 +42,57 @@ typedef struct {
     Position height;
 } ObjectBorder;
 
-static GPrivate ctx_key;
-
 static mp_int_t
-init_module(mediapipe_t *mp);
+init_callback(mediapipe_t* mp);
 
-static mp_int_t init_callback(mediapipe_t* mp);
-
-static void
-exit_master();
-
-static void *
-create_ctx(mediapipe_t *mp);
+static void*
+create_ctx(mediapipe_t* mp);
 
 static void
-destroy_ctx(void *_ctx);
-
-static gboolean
-create_and_add_meta_to_buffer(GstBuffer *buf, stream_ctx_t *ctx);
-
-static gboolean
-copy(char *dest, GstBuffer *buf, int size);
-
-static gboolean
-progress(GstBuffer *buf1, int *spilt_buf_num, stream_ctx_t *ctx);
-
-static void
-decode_need_data(GstElement *appsrc, guint unused_size, gpointer user_data);
-
-static GstPadProbeReturn
-vaapih264decode_src_callback(GstPad *pad, GstPadProbeInfo *info,
-                             gpointer user_data);
+destroy_ctx(void* _ctx);
 
 static mp_command_t
-mp_teststream_commands[] = {
-    {
-        mp_string("teststream"),
-        MP_MAIN_CONF,
-        NULL,
-        0,
-        0,
-        NULL
-    },
-    mp_null_command
-};
+    mp_teststream_commands[]
+    = {
+        mp_custom_command0("teststream"),
+        mp_null_command
+      };
 
 static mp_module_ctx_t
-mp_teststream_module_ctx = {
-    mp_string("teststream"),
-    create_ctx,
-    NULL,
-    destroy_ctx
-};
+    mp_teststream_module_ctx
+    = {
+        mp_string("teststream"),
+        create_ctx,
+        nullptr,
+        destroy_ctx
+      };
 
 mp_module_t
-mp_teststream_module = {
-    MP_MODULE_V1,
-    &mp_teststream_module_ctx,                /* module context */
-    mp_teststream_commands,                   /* module directives */
-    MP_CORE_MODULE,                    /* module type */
-    NULL,                               /* init master */
-    init_module,                               /* init module */
-    NULL,                               /* keyshot_process*/
-    NULL,                               /* message_process */
-    init_callback,                      /* init_callback */
-    NULL,                               /* netcommand_process */
-    exit_master,                               /* exit master */
-    MP_MODULE_V1_PADDING
-};
+    mp_teststream_module
+    = {
+        MP_MODULE_V1,
+        &mp_teststream_module_ctx, /* module context */
+        mp_teststream_commands, /* module directives */
+        MP_CORE_MODULE, /* module type */
+        nullptr, /* init master */
+        nullptr, /* init module */
+        nullptr, /* keyshot_process*/
+        nullptr, /* message_process */
+        init_callback, /* init_callback */
+        nullptr, /* netcommand_process */
+        nullptr, /* exit master */
+        MP_MODULE_V1_PADDING
+      };
 
-/* --------------------------------------------------------------------------*/
-/**
- * @Synopsis vaapih264dec src callback to transfer meta
- *
- * @Returns
- */
-/* ----------------------------------------------------------------------------*/
-static GstPadProbeReturn
-vaapih264decode_src_callback(GstPad *pad, GstPadProbeInfo *info,
-                             gpointer user_data)
+static gboolean
+myconvert_src_callback(mediapipe_t* mp, GstBuffer* buffer, guint8* data, gsize size, gpointer user_data)
 {
     auto ctx = (stream_ctx_t*)user_data;
     auto queue = ctx->meta_queue;
 
     if (g_queue_is_empty(queue)) {
         LOG_WARNING("roi_meta is NULL!");
-        return GST_PAD_PROBE_OK;
+        return TRUE;
     }
 
     guint frameId = 0;
@@ -190,10 +102,8 @@ vaapih264decode_src_callback(GstPad *pad, GstPadProbeInfo *info,
     if (!gst_structure_get_uint(s, "frameId", &frameId)) {
         LOG_WARNING("unexpected GstStructure object");
         gst_structure_free(s);
-        return GST_PAD_PROBE_OK;
+        return TRUE;
     }
-
-    GstBuffer *buffer = GST_PAD_PROBE_INFO_BUFFER(info);
 
     while (TRUE) {
         if (!gst_structure_get_uint(s, "left", &left)) {
@@ -217,7 +127,7 @@ vaapih264decode_src_callback(GstPad *pad, GstPadProbeInfo *info,
             break;
         }
 
-        GstVideoRegionOfInterestMeta *meta = gst_buffer_add_video_region_of_interest_meta(
+        GstVideoRegionOfInterestMeta* meta = gst_buffer_add_video_region_of_interest_meta(
             buffer, "label", left, top, width, height);
         gst_video_region_of_interest_meta_add_param(meta, s);
 
@@ -237,86 +147,22 @@ vaapih264decode_src_callback(GstPad *pad, GstPadProbeInfo *info,
         }
     }
 
-    return GST_PAD_PROBE_OK;
+    return TRUE;
 }
 
-
-/* --------------------------------------------------------------------------*/
-/**
- * @Synopsis  decode pipeline appsrc need-data signal responding func
- *
- * @Param appsrc
- * @Param unused_size
- * @Param user_data
- */
-/* ----------------------------------------------------------------------------*/
-
-static mp_int_t
-init_module(mediapipe_t *mp)
+static void*
+create_ctx(mediapipe_t* mp)
 {
     g_assert(mp != NULL);
-    GstElement *h264decode = NULL;
-    GstElement *Videoconvert = NULL;
-    GstPad *h264srcpad = NULL;
-    GstPad *convertpad = NULL;
-    GstStateChangeReturn ret =  GST_STATE_CHANGE_SUCCESS;
-    stream_ctx_t *ctx = (stream_ctx_t *)mp_modules_find_moudle_ctx(mp, "teststream");
-    ctx->decode_pipeline = mp->pipeline;
-    if (ctx->decode_pipeline == NULL) {
-        LOG_ERROR("teststream: create pipeline failed !");
-        return  MP_ERROR;
-    }
-
-    //Add callback to vaapih264dec
-    h264decode = gst_bin_get_by_name(GST_BIN(ctx->decode_pipeline), "myconvert");
-    if (h264decode == NULL) {
-        LOG_ERROR("can't find element parse");
-        return MP_IGNORE;
-    }
-    h264srcpad = gst_element_get_static_pad(h264decode, "src");
-    if (h264srcpad) {
-        gst_pad_add_probe(h264srcpad, GST_PAD_PROBE_TYPE_BUFFER,
-                          vaapih264decode_src_callback, ctx,
-                          NULL);
-        gst_object_unref(h264srcpad);
-    }
-    gst_object_unref(h264decode);
-
-    //run pipeline
-    ret = gst_element_set_state(ctx->decode_pipeline, GST_STATE_PLAYING);
-    if (ret == GST_STATE_CHANGE_FAILURE) {
-        return MP_ERROR;
-    }
-    if (ret == GST_STATE_CHANGE_FAILURE) {
-        return MP_ERROR;
-    }
-    //new a queue
-    if (NULL == ctx->meta_queue) {
-        ctx->meta_queue = g_queue_new();
-    }
-    return MP_OK;
-}
-
-static void
-exit_master()
-{
-}
-
-static void *
-create_ctx(mediapipe_t *mp)
-{
-    g_assert(mp != NULL);
-    stream_ctx_t *ctx = g_new0(stream_ctx_t, 1);
+    stream_ctx_t* ctx = g_new0(stream_ctx_t, 1);
+    ctx->meta_queue = g_queue_new();
     return ctx;
 }
 
 static void
-destroy_ctx(void *_ctx)
+destroy_ctx(void* _ctx)
 {
-    stream_ctx_t *ctx = (stream_ctx_t *) _ctx;
-    if (ctx->half_buffer) {
-        gst_buffer_unref(ctx->half_buffer);
-    }
+    stream_ctx_t* ctx = (stream_ctx_t*)_ctx;
     if (ctx->meta_queue) {
         g_queue_free_full(ctx->meta_queue, g_free);
     }
@@ -343,9 +189,9 @@ xlinksrc_src_callback(mediapipe_t* mp, GstBuffer* buffer, guint8* data, gsize si
     auto headerSize = header->meta_size + sizeof(header);
 
     for (int i = 0; i < metaData->of_objects; i++) {
-        GstVideoRegionOfInterestMeta *meta = gst_buffer_add_video_region_of_interest_meta(
+        GstVideoRegionOfInterestMeta* meta = gst_buffer_add_video_region_of_interest_meta(
             buffer, "label", border[i].left.u, border[i].top.u, border[i].width.u, border[i].height.u);
-        GstStructure *s = gst_structure_new(
+        GstStructure* s = gst_structure_new(
             "detection", "reserved", G_TYPE_UINT, border[i].reserved, "object_id", G_TYPE_UINT, border[i].object_id,
             "classfication_GT", G_TYPE_UINT, border[i].classfication_GT, "frameId", G_TYPE_UINT, frameId, "left", G_TYPE_UINT, border[i].left.u,
             "top", G_TYPE_UINT, border[i].top.u, "width", G_TYPE_UINT, border[i].width.u, "height", G_TYPE_UINT, border[i].height.u, NULL);
@@ -367,29 +213,10 @@ xlinksrc_src_callback(mediapipe_t* mp, GstBuffer* buffer, guint8* data, gsize si
     return TRUE;
 }
 
-#define GST_VIDEO_REGION_OF_INTEREST_META_ITERATE(buf, state)                                                          \
-    ((GstVideoRegionOfInterestMeta *)gst_buffer_iterate_meta_filtered(buf, state,                                      \
-                                                                      GST_VIDEO_REGION_OF_INTEREST_META_API_TYPE))
-
-static gboolean
-gvaclassify_sink_callback(mediapipe_t* mp, GstBuffer* buffer, guint8* data, gsize size, gpointer user_data)
-{
-    std::vector<GstVideoRegionOfInterestMeta *> metas;
-    GstVideoRegionOfInterestMeta *meta = NULL;
-    gpointer state = NULL;
-    while ((meta = GST_VIDEO_REGION_OF_INTEREST_META_ITERATE(buffer, &state))) {
-        metas.push_back(meta);
-    }
-
-    LOG_INFO("size=%lu", metas.size());
-
-    return TRUE;
-}
-
 static mp_int_t init_callback(mediapipe_t* mp)
 {
     auto ctx = mp_modules_find_moudle_ctx(mp, "teststream");
     mediapipe_set_user_callback(mp, "src", "src", xlinksrc_src_callback, ctx);
-    mediapipe_set_user_callback(mp, "classify", "sink", gvaclassify_sink_callback, ctx);
+    mediapipe_set_user_callback(mp, "myconvert", "src", myconvert_src_callback, ctx);
     return MP_OK;
 }
