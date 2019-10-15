b40162e4c98555bee4f1a23007e76e4a763cb061
Hu, Yuan2 <yuan2.hu@intel.com>
2019-10-15 12:21:12 +0800

    remove some codes confict with ip plan and not used code
    not used code
    remove hddl_mediapipe3
    remove hddl_mediapipe
    remove network_service
    
    confict code
    remove test code about dma allocator
    
    Signed-off-by: Hu, Yuan2 <yuan2.hu@intel.com>

--- front/network_service/lib/constants.js
+++
@@ -1,7 +0,0 @@
-//Copyright (C) 2019 Intel Corporation
-// 
-//SPDX-License-Identifier: MIT
-//
-module.exports = Object.freeze({
-    msgType: {"ePipeCreate":0, "ePipeID":1, "eConfig": 2,"eLaunch": 3, "eProperty":4, "ePipeDestroy": 5, "eMetaText": 6, "eMetaJPG":7}
-});
