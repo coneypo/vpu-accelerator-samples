cb62cfee0af14d7bbdee811a9f2843e6af976648
Kezhen <kezhen.liu@intel.com>
2020-01-02 18:08:20 +0800

    checkpoint 202001021808

--- inc/jpeg_enc_node.hpp
+++ inc/jpeg_enc_node.hpp
@@ -29,12 +29,14 @@ public:
     ~VaapiSurfaceAllocator();
     bool alloc(const VaapiSurfaceAllocatorConfig& config, VASurfaceID surfaces[]);
     void free();
+    bool getSurfacesAddr(VASurfaceID*& surfAddr);
 
     VaapiSurfaceAllocator& operator=(const VaapiSurfaceAllocator&) = delete;
     VaapiSurfaceAllocator& operator=(VaapiSurfaceAllocator&&) = delete;
 
 private:
     VADisplay* m_dpy;
+    VAContextID* m_ctx;
     std::vector<VASurfaceID> m_surfaces;
 };
 
@@ -43,28 +45,36 @@ public:
     struct Surface;
     struct Surface{
         VASurfaceID surfaceId;
+        std::size_t index;
         Surface* next;
     };
 
     SurfacePool(VADisplay* dpy, VaapiSurfaceAllocator* allocator);
     ~SurfacePool();
     bool init(VaapiSurfaceAllocator::Config& config);
-    bool getFreeSurface(Surface& surface);
-    bool tryGetFreeSurface(Surface& surface);
-    bool moveToUsed();
+    bool getFreeSurface(Surface*& surface);
+    bool tryGetFreeSurface(Surface*& surface);
+    bool moveToUsed(Surface*& surface);
+    bool getUsedSurface(Surface*& surface);
 private:
-    bool getFreeSurfaceUnsafe(Surface& surface);
+    bool getFreeSurfaceUnsafe(Surface*& surface);
+    bool moveToUsedUnsafe(Surface*& surface);
+    bool getUsedSurfaceUnsafe(Surface*& surface);
 
     Surface* m_freeSurfaces;
     Surface* m_usedSurfaces;
     VADisplay* m_dpy;
+    VAContextID* m_ctx;
     VaapiSurfaceAllocator* m_allocator;
     std::mutex m_mutex;
     std::condition_variable m_cv;
+    std::mutex m_usedListMutex;
 };
 
 struct JpegEncPicture{
-    VASurfaceID surfaceId;
+    std::size_t index;
+
+    // VASurfaceID surfaceId;
     VABufferID codedBufId;
     VABufferID picParamBufId;
     VABufferID qMatrixBufId;
@@ -73,12 +83,12 @@ struct JpegEncPicture{
     VABufferID headerParamBufId;
     VABufferID headerDataBufId;
 
-    VAEncPictureParameterBufferJPEG picParam;
-    VAQMatrixBufferJPEG qMatrix;
-    VAQMatrixBufferJPEG huffmanTbl;
-    VAEncSliceParameterBufferJPEG sliceParam;
-    VAEncPackedHeaderParameterBuffer headerParam;
-    unsigned char* headerData;
+    // VAEncPictureParameterBufferJPEG picParam;
+    // VAQMatrixBufferJPEG qMatrix;
+    // VAQMatrixBufferJPEG huffmanTbl;
+    // VAEncSliceParameterBufferJPEG sliceParam;
+    // VAEncPackedHeaderParameterBuffer headerParam;
+    // unsigned char* headerData;
 
 };
 
@@ -92,33 +102,170 @@ struct JpegEncPicture{
 
 // };
 
+inline std::size_t alignTo(std::size_t s) { //to-do: adjust alignment here
+
+    return ((s + 16 - 1) & (~15));
+};
+
+class Defaults{
+public:
+    Defaults(const Defaults&) = delete;
+    Defaults& operator=(const Defaults&) = delete;
+    
+    static const Defaults& getDefaults(){
+        static Defaults inst;
+        return inst;
+    };
+    VAQMatrixBufferJPEG* qMatrix() const { return &m_qMatrixParam; };
+    VAHuffmanTableBufferJPEGBaseline* huffmanTbl() const { return &m_huffmanTbl; };
+private:
+    Defaults();
+
+    // for quantization matrix
+    void jpegenc_qmatrix_init();
+    const int NUM_QUANT_ELEMENTS;
+    const uint8_t* jpeg_luma_quant;
+    const uint8_t* jpeg_zigzag;
+    const uint8_t* jpeg_chroma_quant;
+    VAQMatrixBufferJPEG m_qMatrixParam;
+
+    // for huffman table
+    void jpegenc_hufftable_init();
+    VAHuffmanTableBufferJPEGBaseline m_huffmanTbl;
+};
+
+Defaults::Defaults(): NUM_QUANT_ELEMENTS(64){
+    jpeg_luma_quant = new uint8_t[NUM_QUANT_ELEMENTS]{
+        16, 11, 10, 16, 24,  40,  51,  61,
+        12, 12, 14, 19, 26,  58,  60,  55,
+        14, 13, 16, 24, 40,  57,  69,  56,
+        14, 17, 22, 29, 51,  87,  80,  62,
+        18, 22, 37, 56, 68,  109, 103, 77,
+        24, 35, 55, 64, 81,  104, 113, 92,
+        49, 64, 78, 87, 103, 121, 120, 101,
+        72, 92, 95, 98, 112, 100, 103, 99 
+    };
+
+    jpeg_zigzag = new uint8_t[]{
+        0,   1,   8,   16,  9,   2,   3,   10,
+        17,  24,  32,  25,  18,  11,  4,   5,
+        12,  19,  26,  33,  40,  48,  41,  34,
+        27,  20,  13,  6,   7,   14,  21,  28,
+        35,  42,  49,  56,  57,  50,  43,  36,
+        29,  22,  15,  23,  30,  37,  44,  51,
+        58,  59,  52,  45,  38,  31,  39,  46,
+        53,  60,  61,  54,  47,  55,  62,  63
+    };
+
+    jpeg_chroma_quant = new uint8_t[NUM_QUANT_ELEMENTS] = {
+        17, 18, 24, 47, 99, 99, 99, 99,
+        18, 21, 26, 66, 99, 99, 99, 99,
+        24, 26, 56, 99, 99, 99, 99, 99,
+        47, 66, 99, 99, 99, 99, 99, 99,
+        99, 99, 99, 99, 99, 99, 99, 99,
+        99, 99, 99, 99, 99, 99, 99, 99,
+        99, 99, 99, 99, 99, 99, 99, 99,
+        99, 99, 99, 99, 99, 99, 99, 99
+    };
+
+    jpegenc_qmatrix_init();
+};
+
+Defaults::~Defaults(){
+    delete[] jpeg_luma_quant;
+    delete[] jpeg_zigzag;
+    delete[] jpeg_chroma_quant;
+};
+
+void Defaults::jpegenc_qmatrix_init(){
+    m_qMatrixParam.load_lum_quantiser_matrix = 1;
+   
+    //LibVA expects the QM in zigzag order 
+    for(int i=0; i<NUM_QUANT_ELEMENTS; i++) {
+        m_qMatrixParam.lum_quantiser_matrix[i] = jpeg_luma_quant[jpeg_zigzag[i]];
+    }
+    
+    //to-do: configuration below does not consider all circumstances
+    m_qMatrixParam.load_chroma_quantiser_matrix = 1;
+    for(int i=0; i<NUM_QUANT_ELEMENTS; i++) {
+        m_qMatrixParam.chroma_quantiser_matrix[i] = jpeg_chroma_quant[jpeg_zigzag[i]];
+    }
+    
+};
+
+void Defaults::jpegenc_hufftable_init(){
+    
+    m_huffmanTbl.load_huffman_table[0] = 1; //Load Luma Hufftable
+    m_huffmanTbl.load_huffman_table[1] = 1; //Load Chroma Hufftable for other formats
+    
+   //Load Luma hufftable values
+   //Load DC codes
+   memcpy(hufftable_param->huffman_table[0].num_dc_codes, jpeg_hufftable_luma_dc+1, 16);
+   //Load DC Values
+   memcpy(hufftable_param->huffman_table[0].dc_values, jpeg_hufftable_luma_dc+17, 12);
+   //Load AC codes
+   memcpy(hufftable_param->huffman_table[0].num_ac_codes, jpeg_hufftable_luma_ac+1, 16);
+   //Load AC Values
+   memcpy(hufftable_param->huffman_table[0].ac_values, jpeg_hufftable_luma_ac+17, 162);
+   memset(hufftable_param->huffman_table[0].pad, 0, 2);
+      
+   
+   //Load Chroma hufftable values if needed
+   if(yuvComp.fourcc_val != VA_FOURCC_Y800) {
+       //Load DC codes
+       memcpy(hufftable_param->huffman_table[1].num_dc_codes, jpeg_hufftable_chroma_dc+1, 16);
+       //Load DC Values
+       memcpy(hufftable_param->huffman_table[1].dc_values, jpeg_hufftable_chroma_dc+17, 12);
+       //Load AC codes
+       memcpy(hufftable_param->huffman_table[1].num_ac_codes, jpeg_hufftable_chroma_ac+1, 16);
+       //Load AC Values
+       memcpy(hufftable_param->huffman_table[1].ac_values, jpeg_hufftable_chroma_ac+17, 162);
+       memset(hufftable_param->huffman_table[1].pad, 0, 2);      
+       
+   }
+    
+}
+
 class JpegEncNode : public hva::hvaNode_t{
+    friend class JpegEncNodeWorker;
 public:
     JpegEncNode(std::size_t inPortNum, std::size_t outPortNum, std::size_t totalThreadNum);
+
+    ~JpegEncNode();
 
     virtual std::shared_ptr<hva::hvaNodeWorker_t> createNodeWorker() const override;
 
     bool initVaapi();
 
-private:
-    bool initVaapiCallOnce();
+    bool initVaJpegCtx();
 
+private:
     VADisplay m_vaDpy;
     int m_vaMajorVer;
-    int m_vaMinorVer
+    int m_vaMinorVer;
     VAConfigID m_jpegConfigId;
+    VAContextID m_jpegCtxId;
+    VaapiSurfaceAllocator* m_allocator;
+    SurfacePool* m_pool;
+    bool m_ready;
+    unsigned m_picWidth;
+    unsigned m_picHeight;
+    JpegEncPicture* m_picPool;
 };
 
 class JpegEncNodeWorker : public hva::hvaNodeWorker_t{
 public:
+
     JpegEncNodeWorker(hva::hvaNode_t* parentNode);
 
     virtual void process(std::size_t batchIdx) override;
 
     virtual void init() override;
 
-private:
+    virtual void deinit() override;
 
-};
+private:
+    bool prepareSurface(VASurfaceID surface, const unsigned char* img);
+    void jpegenc_pic_param_init(VAEncPictureParameterBufferJPEG *pic_param,int width,int height,int quality);
 
 #endif //#ifndef JPEG_ENC_NODE_HPP
--- src/jpeg_enc_node.cpp
+++ src/jpeg_enc_node.cpp
@@ -1,13 +1,13 @@
 #include <jpeg_enc_node.hpp>
 #include <va_common/va_display.h>
 
-VaapiSurfaceAllocator::VaapiSurfaceAllocator(VADisplay* dpy):m_dpy(dpy){ };
+VaapiSurfaceAllocator::VaapiSurfaceAllocator(VADisplay* dpy):m_dpy(dpy){ }
 
 VaapiSurfaceAllocator::~VaapiSurfaceAllocator(){
     if(m_surfaces.size()){
         free();
     }
- };
+ }
 
 bool VaapiSurfaceAllocator::alloc(const VaapiSurfaceAllocatorConfig& config, VASurfaceID surfaces[]){
     if(!m_dpy){
@@ -27,15 +27,17 @@ bool VaapiSurfaceAllocator::alloc(const VaapiSurfaceAllocatorConfig& config, VAS
         std::cout<<"Failed to allocate surfaces: "<<va_status<<std::endl;
         return false;
     }
-    else{
-        if(m_surfaces.capacity() < m_surfaces.size()+ config.surfaceNum){
-            m_surfaces.reserve(m_surfaces.size()+ config.surfaceNum);
-        }
-        for(std::size_t i =0; i<config.surfaceNum;++i){
-            m_surfaces.push_back(surfaces[i]);
-        }
-        return true;
+
+    // va_status = vaCreateContext(*m_dpy,)
+
+    if(m_surfaces.capacity() < m_surfaces.size()+ config.surfaceNum){
+        m_surfaces.reserve(m_surfaces.size()+ config.surfaceNum);
     }
+    for(std::size_t i =0; i<config.surfaceNum;++i){
+        m_surfaces.push_back(surfaces[i]);
+    }
+    return true;
+    
 }
 
 void VaapiSurfaceAllocator::free(){
@@ -43,6 +45,14 @@ void VaapiSurfaceAllocator::free(){
         vaDestroySurfaces(*m_dpy,&(*it),1);
     }
     std::vector<VASurfaceID>().swap(m_surfaces);
+}
+
+bool VaapiSurfaceAllocator::getSurfacesAddr(VASurfaceID*& surfAddr){
+    if(m_surfaces.empty())
+        return false;
+
+    surfAddr = &(m_surfaces[0]);
+    return true;
 }
 
 SurfacePool::SurfacePool(VADisplay* dpy, VaapiSurfaceAllocator* allocator): m_dpy(dpy),m_allocator(allocator),
@@ -69,10 +79,10 @@ bool SurfacePool::init(VaapiSurfaceAllocator::Config& config){
     VASurfaceID surfaces[config.surfaceNum];
     if(m_allocator->alloc(config, surfaces)){
         for(std::size_t i = 0; i < config.surfaceNum; ++i){
-            Surface* newItem = new Surface{surfaces[i], m_freeSurfaces};
+            Surface* newItem = new Surface{surfaces[i], i, m_freeSurfaces};
             m_freeSurfaces = newItem;
-            return true;
         }
+        return true;
     }
     else{
         std::cout<<"Fail to allocate surfaces in surface pool"<<std::endl;
@@ -80,14 +90,14 @@ bool SurfacePool::init(VaapiSurfaceAllocator::Config& config){
     }
 }
 
-bool SurfacePool::getFreeSurfaceUnsafe(SurfacePool::Surface& surface){
-    surface = *m_freeSurfaces;
-    m_freeSurfaces = surface.next;
-    surface.next = nullptr;
+bool SurfacePool::getFreeSurfaceUnsafe(SurfacePool::Surface*& surface){
+    surface = m_freeSurfaces;
+    m_freeSurfaces = surface->next;
+    surface->next = nullptr;
     return true;
 }
 
-bool SurfacePool::getFreeSurface(SurfacePool::Surface& surface){
+bool SurfacePool::getFreeSurface(SurfacePool::Surface*& surface){
     std::unique_lock<std::mutex> lk(m_mutex);
     m_cv.wait(lk, [&]{return m_freeSurfaces!=nullptr;});
     getFreeSurfaceUnsafe(surface);
@@ -95,7 +105,7 @@ bool SurfacePool::getFreeSurface(SurfacePool::Surface& surface){
     return true;
 }
 
-bool SurfacePool::tryGetFreeSurface(SurfacePool::Surface& surface){
+bool SurfacePool::tryGetFreeSurface(SurfacePool::Surface*& surface){
     std::lock_guard<std::mutex> lg(m_mutex);
     if(m_freeSurfaces){
         return getFreeSurfaceUnsafe(surface);
@@ -104,29 +114,71 @@ bool SurfacePool::tryGetFreeSurface(SurfacePool::Surface& surface){
         return false;
 }
 
+bool SurfacePool::moveToUsed(SurfacePool::Surface*& surface){
+    std::lock_guard<std::mutex> lg(m_usedListMutex);
+    return moveToUsedUnsafe(surface);
+}
+
+bool SurfacePool::moveToUsedUnsafe(SurfacePool::Surface*& surface){
+    surface->next = m_usedSurfaces;
+    m_usedSurfaces = surface;
+    return true;
+}
+
+bool SurfacePool::getUsedSurface(SurfacePool::Surface*& surface){
+    std::lock_guard<std::mutex> lg(m_usedListMutex);
+    return getUsedSurfaceUnsafe(surface);
+}
+
+bool SurfacePool::getUsedSurfaceUnsafe(SurfacePool::Surface*& surface){
+    if(!m_usedSurfaces){
+        surface = nullptr;
+        return true;
+    }
+    else{
+        surface = m_usedSurfaces;
+        m_usedSurfaces = surface->next;
+        surface->next = nullptr;
+        return true;
+    }
+}
+
 
 JpegEncNode::JpegEncNode(std::size_t inPortNum, std::size_t outPortNum, std::size_t totalThreadNum):
-        hva::hvaNode_t(inPortNum, outPortNum, totalThreadNum){
+        hva::hvaNode_t(inPortNum, outPortNum, totalThreadNum), m_ready(false), m_picHeight(0u), m_picWidth(0u){
+    if(!initVaapi()){
+        std::cout<<"Vaapi init failed"<<std::endl;
+        return;
+    }
+    m_allocator = new VaapiSurfaceAllocator(&m_vaDpy); //not allocated any surfaces yet
+    m_pool = new SurfacePool(&m_vaDpy, m_allocator); //not init yet
+    m_picPool = new JpegEncPicture[16];
+    memset(m_picPool, 0, sizeof(JpegEncPicture)*16);
+}
 
+JpegEncNode::~JpegEncNode(){
+    if(!m_allocator)
+        delete m_allocator;
+
+    if(!m_pool)
+        delete m_pool;
+
+    if(!m_picPool)
+        delete[] m_picPool;
+
+    vaDestroyConfig(m_vaDpy,m_jpegConfigId);
+    vaTerminate(m_vaDpy);
+    va_close_display(m_vaDpy);
 }
 
 std::shared_ptr<hva::hvaNodeWorker_t> JpegEncNode::createNodeWorker() const {
     return std::shared_ptr<hva::hvaNodeWorker_t>(new JpegEncNodeWorker((JpegEncNode*)this) );
 }
 
-JpegEncNodeWorker::JpegEncNodeWorker(hva::hvaNode_t* parentNode):
-        hva::hvaNodeWorker_t(parentNode){
-
-}
-
-void JpegEncNodeWorker::init(){
-
-}
-
-bool JpegEncNode::initVaapiCallOnce(){
+bool JpegEncNode::initVaapi(){
 
     /* 1. Initialize the va driver */
-    m_dpy = va_open_display();
+    m_vaDpy = va_open_display();
 
     VAStatus va_status = vaInitialize(m_vaDpy, &m_vaMajorVer, &m_vaMinorVer);
     if(va_status != VA_STATUS_SUCCESS){
@@ -137,13 +189,13 @@ bool JpegEncNode::initVaapiCallOnce(){
     /* 2. Query for the entrypoints for the JPEGBaseline profile */
     VAEntrypoint entrypoints[5];
     int num_entrypoints = -1;
-    va_status = vaQueryConfigEntrypoints(va_dpy, VAProfileJPEGBaseline, entrypoints, &num_entrypoints);
+    va_status = vaQueryConfigEntrypoints(m_vaDpy, VAProfileJPEGBaseline, entrypoints, &num_entrypoints);
     if(va_status != VA_STATUS_SUCCESS){
         std::cout<<"vaQueryConfigEntrypoints failed"<<std::endl;
         return false;
     }    // We need picture level encoding (VAEntrypointEncPicture). Find if it is supported. 
-    int enc_entrypoint = 0
-    for (; enc_entrypoint < num_entrypoints; enc_entrypoint++) {
+    int enc_entrypoint = 0;
+    for(; enc_entrypoint < num_entrypoints; enc_entrypoint++) {
         if (entrypoints[enc_entrypoint] == VAEntrypointEncPicture)
             break;
     }
@@ -181,8 +233,183 @@ bool JpegEncNode::initVaapiCallOnce(){
     
     /* 4. Create Config for the profile=VAProfileJPEGBaseline, entrypoint=VAEntrypointEncPicture,
      * with RT format attribute */
-    va_status = vaCreateConfig(va_dpy, VAProfileJPEGBaseline, VAEntrypointEncPicture, 
+    va_status = vaCreateConfig(m_vaDpy, VAProfileJPEGBaseline, VAEntrypointEncPicture, 
                                &attrib[0], 2, &m_jpegConfigId);
-    CHECK_VASTATUS(va_status, "vaQueryConfigEntrypoints");
+    if(va_status != VA_STATUS_SUCCESS){
+        std::cout<<"VA Create Config failed"<<std::endl;
+        return false;
+    }
+    return true;
+}
 
+bool JpegEncNode::initVaJpegCtx(){
+    VASurfaceID* surfAddr = nullptr;
+    m_allocator->getSurfacesAddr(surfAddr);
+    if(!m_vaDpy || !m_jpegConfigId){
+        std::cout<<"VA display or va config unset!"<<std::endl;
+        return false;
+    }
+    if(!m_picWidth || !m_picHeight){
+        std::cout<<"Pic Width and Pic Height unset!"<<std::endl;
+        return false;
+    }
+    VAStatus va_status = vaCreateContext(m_vaDpy, m_jpegConfigId, m_picWidth, m_picHeight, 
+                                VA_PROGRESSIVE, surfAddr, 16, &m_jpegCtxId);
+    if(va_status != VA_STATUS_SUCCESS){
+        std::cout<<"Create va context failed!"<<std::endl;
+        return false;
+    }
+    return true;
+}
+
+
+JpegEncNodeWorker::JpegEncNodeWorker(hva::hvaNode_t* parentNode):
+        hva::hvaNodeWorker_t(parentNode){
+
+}
+
+void JpegEncNodeWorker::init(){
+
+}
+
+struct InfoROI_t {
+    int widthImage = 0;
+    int heightImage = 0;
+    int x = 0;
+    int y = 0;
+    int width = 0;
+    int height = 0;
+    int indexROI = 0;
+    int totalROINum = 0;
+    int frameId = 0;
+};
+
+void JpegEncNodeWorker::process(std::size_t batchIdx){
+    std::vector<std::shared_ptr<hva::hvaBlob_t>> vInput= hvaNodeWorker_t::getParentPtr()->getBatchedInput(batchIdx, std::vector<size_t> {0});
+    if(vInput.size()==0u){
+        //to-do: do usedsufaces sync here
+        return;
+    }
+    if(!((JpegEncNode*)m_parentNode)->m_ready){
+        InfoROI_t* meta = vInput[0]->get<unsigned char, InfoROI_t>(0)->getMeta();
+        if(!meta->widthImage || !meta->heightImage){
+            std::cout<<"Picture width or height unset!"<<std::endl;
+            return;
+        }
+        else{
+            VaapiSurfaceAllocator::Config config = {VA_RT_FORMAT_YUV420, 0u, 0u, 16u}; //To-do: make surfaces num virable
+            ((JpegEncNode*)m_parentNode)->m_picWidth = config.width = meta->widthImage;
+            ((JpegEncNode*)m_parentNode)->m_picHeight = config.height = meta->heightImage;
+            if(!((JpegEncNode*)m_parentNode)->m_pool->init(config)){
+                std::cout<<"Surface pool init failed!"<<std::endl;
+                return;
+            }
+            VASurfaceID* surfAddr = nullptr;
+            if(!((JpegEncNode*)m_parentNode)->initVaJpegCtx()){
+                std::cout<<"Init jpeg context failed!"<<std::endl;
+                return;
+            }
+            ((JpegEncNode*)m_parentNode)->m_ready = true;
+        }
+    }
+
+    SurfacePool::Surface* surface = nullptr;
+    if(((JpegEncNode*)m_parentNode)->m_pool->tryGetFreeSurface(surface)){
+        VASurfaceID vaSurf = surface->surfaceId;
+        if(!vaSurf){
+            std::cout<<"Surface got is invalid!"<<std::endl;
+        }
+        const unsigned char* img = vInput[0]->get<unsigned char, std::pair<unsigned, unsigned>>(1)->getPtr();
+        if(!prepareSurface(vaSurf, img)){
+            std::cout<<"Fail to prepare input surface!"<<std::endl;
+            return;
+        }
+        unsigned picWidth = ((JpegEncNode*)m_parentNode)->m_picWidth;
+        unsigned picHeight = ((JpegEncNode*)m_parentNode)->m_picHeight;
+        JpegEncPicture* picPool = ((JpegEncNode*)m_parentNode)->m_picPool;
+        std::size_t index = surface->index;
+        // Encoded buffer
+        VAStatus va_status = vaCreateBuffer(((JpegEncNode*)m_parentNode)->m_vaDpy, ((JpegEncNode*)m_parentNode)->m_jpegCtxId,
+                VAEncCodedBufferType, picWidth*picHeight*3/2, 1, NULL, &(picPool[index].codedBufId));
+        if(va_status != VA_STATUS_SUCCESS){
+            std::cout<<"Create coded buffer failed!"<<std::endl;
+            return;
+        }
+
+        // Picture parameter buffer
+        VAEncPictureParameterBufferJPEG pic_param;
+        memset(&pic_param, 0, sizeof(pic_param));
+        pic_param.coded_buf = picPool[index].codedBufId;
+        jpegenc_pic_param_init(&pic_param, picWidth, picHeight, 50); //to-do: make quality virable
+        va_status = vaCreateBuffer(((JpegEncNode*)m_parentNode)->m_vaDpy, ((JpegEncNode*)m_parentNode)->m_jpegCtxId,
+                VAEncPictureParameterBufferType, sizeof(VAEncPictureParameterBufferJPEG), 1, &pic_param, &(picPool[index].picParamBufId));
+        if(va_status != VA_STATUS_SUCCESS){
+            std::cout<<"Create picture param failed!"<<std::endl;
+            return;
+        }
+
+        //Quantization matrix
+        va_status = vaCreateBuffer(((JpegEncNode*)m_parentNode)->m_vaDpy, ((JpegEncNode*)m_parentNode)->m_jpegCtxId,
+                VAQMatrixBufferType, sizeof(VAQMatrixBufferJPEG), 1, Defaults::getDefaults().qMatrix, &(picPool[index].qMatrixBufId));
+        if(va_status != VA_STATUS_SUCCESS){
+            std::cout<<"Create quantization matrix failed!"<<std::endl;
+            return;
+        }
+
+
+    }
+    else{
+        // to-do: sync on usedsurfaces
+    }
+
+}
+
+void JpegEncNodeWorker::jpegenc_pic_param_init(VAEncPictureParameterBufferJPEG *pic_param,int width,int height,int quality){
+    pic_param->picture_width = width;
+    pic_param->picture_height = height;
+    pic_param->quality = quality;
+    
+    pic_param->pic_flags.bits.profile = 0;      //Profile = Baseline
+    pic_param->pic_flags.bits.progressive = 0;  //Sequential encoding
+    pic_param->pic_flags.bits.huffman = 1;      //Uses Huffman coding
+    pic_param->pic_flags.bits.interleaved = 0;  //Input format is interleaved (YUV)
+    pic_param->pic_flags.bits.differential = 0; //non-Differential Encoding
+    
+    pic_param->sample_bit_depth = 8; //only 8 bit sample depth is currently supported
+    pic_param->num_scan = 1;
+    pic_param->num_components = 3; // Supporting only upto 3 components maximum
+    //set component_id Ci and Tqi
+    
+    //to-do: the conf below does not consider all circumstances
+    pic_param->component_id[0] = pic_param->quantiser_table_selector[0] = 0;
+    pic_param->component_id[1] = pic_param->quantiser_table_selector[1] = 1;
+    pic_param->component_id[2] = 2; pic_param->quantiser_table_selector[2] = 1;
+    
+    pic_param->quality = quality;
+}
+
+bool JpegEncNodeWorker::prepareSurface(VASurfaceID surface, const unsigned char* img){
+    if(!surface || !img){
+        std::cout<<"Invalid input. Fail to prepare input surface"<<std::endl;
+        return false;
+    }
+    VAImage vaImg;
+    VAStatus va_status = vaDeriveImage(((JpegEncNode*)m_parentNode)->m_vaDpy, surface, &vaImg);
+    if(va_status != VA_STATUS_SUCCESS){
+        std::cout<<"Fail to derive image."<<std::endl;
+        return false;
+    }
+    void* mappedSurf = nullptr;
+    va_status = vaMapBuffer(((JpegEncNode*)m_parentNode)->m_vaDpy,vaImg.buf, &mappedSurf);
+    if(va_status != VA_STATUS_SUCCESS){
+        std::cout<<"Fail to map image."<<std::endl;
+        return false;
+    }
+    unsigned picWidth = ((JpegEncNode*)m_parentNode)->m_picWidth;
+    unsigned picHeight = ((JpegEncNode*)m_parentNode)->m_picHeight;
+    // unsigned offset = picWidth * alignTo(picHeight);
+    memcpy(mappedSurf, img, (unsigned)(picWidth*picHeight*3/2)); // to-do: currently no alignment
+    // memcpy(mappedSurf+offset, img+offset, (unsigned)(picWidth * picHeight /2));
+    vaUnmapBuffer(((JpegEncNode*)m_parentNode)->m_vaDpy,vaImg.buf);
+    vaDestroyImage(((JpegEncNode*)m_parentNode)->m_vaDpy,vaImg.image_id);
 }
