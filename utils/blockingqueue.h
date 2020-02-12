a1063bfa831a8054d0e68f24847bf77e3da8a2fb
Liu Xiao <xiao.x.liu@intel.com>
2020-02-13 01:35:09 +0800

    Update CMakefileList
    Signed-off-by: Liu Xiao <xiao.x.liu@intel.com>

--- utils/blockingqueue.h
+++ utils/blockingqueue.h
@@ -1,43 +1,45 @@
 #ifndef __GST_BLOCKINGQUEUE_H__
 #define __GST_BLOCKINGQUEUE_H__
 
-#include <queue>
 #include <condition_variable>
-
+#include <queue>
 
 template <typename T>
 class BlockingQueue {
 
 public:
-  BlockingQueue(): _mutex(), _nonEmpty(), _queue() {}
+    BlockingQueue() = default;
 
-  T take() {
-    std::unique_lock<std::mutex> lock(_mutex);
-    _nonEmpty.wait(lock, [this]{ return !this->_queue.empty(); });
-    T ret(std::move(_queue.front()));
-    _queue.pop();
+    T take()
+    {
+        std::unique_lock<std::mutex> lock(_mutex);
+        _nonEmpty.wait(lock, [this] { return !this->_queue.empty(); });
+        T ret(std::move(_queue.front()));
+        _queue.pop();
+        return ret;
+    }
 
-    return ret;
-  }
+    void put(const T& x)
+    {
+        std::lock_guard<std::mutex> lock(_mutex);
+        _queue.push(x);
+        _nonEmpty.notify_one();
+    }
 
-  void put(const T &x) {
-    std::lock_guard<std::mutex> lock(_mutex);
-    _queue.push(x);
-    _nonEmpty.notify_one();
-  }
+    size_t size()
+    {
+        std::lock_guard<std::mutex> lock(_mutex);
+        return _queue.size();
+    }
 
-  size_t size() {
-    std::lock_guard<std::mutex> lock(_mutex);
-    return _queue.size();
-  }
-
-  BlockingQueue(const BlockingQueue&) = delete;
-  BlockingQueue& operator=(const BlockingQueue&) = delete;
+    BlockingQueue(const BlockingQueue&) = delete;
+    BlockingQueue& operator=(const BlockingQueue&) = delete;
 
 private:
-  std::mutex _mutex;
-  std::condition_variable _nonEmpty;
-  std::queue<T> _queue;
+
+    std::mutex _mutex;
+    std::condition_variable _nonEmpty;
+    std::queue<T> _queue;
 };
 
 #endif
