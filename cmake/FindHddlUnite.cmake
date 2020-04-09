15448c1ff4277c252bd0020a3f7343209ca4ce4e
Kezhen <kezhen.liu@intel.com>
2020-04-09 13:16:23 +0000

    Refined on find hddlunite and added example export script

--- cmake/FindHddlUnite.cmake
+++ cmake/FindHddlUnite.cmake
@@ -1,9 +1,9 @@
 
-find_library (HDDLUNITE_LIB NAMES HddlUnite HINTS "$ENV{INSTALL_DIR}/lib")
+find_library (HDDLUNITE_LIB NAMES HddlUnite HINTS "$ENV{HDDLUNITE_HOST_INSTALL_DIR}/lib")
 # find_library (HDDLUNITE_LIB NAMES HddlUnite HINTS "/home/kmb1/xiaoxi/work/workshop/hddlunite/lib")
 
 # find_path(HDDLUNITE_INCLUDE_DIR NAMES WorkloadContext.h HINTS "/home/kmb1/xiaoxi/work/workshop/hddlunite/include")
-find_path(HDDLUNITE_INCLUDE_DIR NAMES WorkloadContext.h)
+find_path(HDDLUNITE_INCLUDE_DIR NAMES WorkloadContext.h HINTS "$ENV{HDDLUNITE_HOST_INSTALL_DIR}/include")
 # include_directories (${HDDLUNITE_INCLUDE_DIR})
 message ("HddlUnite header directory: ${HDDLUNITE_INCLUDE_DIR}")
 message ("HddlUnite lib: ${HDDLUNITE_LIB}")
--- script/build_env.sh
+++ script/build_env.sh
@@ -6,3 +6,9 @@ export INSTALL_DIR_KMB=/home/cpival/zhicheng/kezhen/installkmb
 export HDDLUNITE_HOST_INSTALL_DIR=/home/cpival/zhicheng/kezhen/hddlunite_install
 export HDDLUNITE_KMB_INSTALL_DIR=/home/cpival/zhicheng/kezhen/hddlunite_installkmb
 export HDDLUNITE_ROOT_DIR=/home/cpival/zhicheng/kezhen/hddlunite
+
+export LD_LIBRARY_PATH=${HDDLUNITE_KMB_INSTALL_DIR}/lib:${INSTALL_DIR}/lib:${INSTALL_DIR}/lib/gstreamer-1.0:${LD_LIBRARY_PATH}
+export PKG_CONFIG_PATH=${INSTALL_DIR}/lib/pkgconfig:$PKG_CONFIG_PATH
+export GST_PLUGIN_PATH=${INSTALL_DIR}/lib:${INSTALL_DIR}/lib/gstreamer-1.0
+export PATH=$INSTALL_DIR/bin/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+export LIBRARY_PATH=$INSTALL_DIR/lib:$INSTALL_DIR/lib/gstreamer-1.0
